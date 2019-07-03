132fd04150c30f9b391803239e8daa6cee6131e7
Ruslan Safin <ruslan.safin@byndyusoft.com>
2019-07-03 22:47:58 +0500

    added dotnetcore tests

---
+++ Backend/UnitTests_TDD/NUnitTestProject1_Core/2 StringTests.cs
@@ -0,0 +1,28 @@
+﻿// ReSharper disable InconsistentNaming
+
+namespace _01CreateUnitTest
+{
+    using System;
+    using NUnit.Framework;
+
+    public class StringTests
+    {
+        [Test]
+        public void StringComparingCaseSensitive()
+        {
+            bool result = "some string" == "some String";
+
+            Assert.False(result);
+        }
+        [Test]
+        public void IfStringIsNull_LenghtThrowsException()
+        {
+            string nullString = null;
+            Assert.Throws<NullReferenceException>(
+                () =>
+                {
+                    nullString.Length.ToString();
+                });
+        }
+    }
+}
